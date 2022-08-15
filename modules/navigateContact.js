6ae3b62c9a5478bc0bd508e21d49ada57ea38894
benardop <benard.oc.p@gmail.com>
2022-08-16 00:56:35 +0300

    Add 'navigateContact.js' file

---
+++ modules/navigateContact.js
@@ -0,0 +1,10 @@
+const navigateAddNew = (list, sectionList, addNew, sectionAddBook, contact, sectionContact) => {
+    sectionList.style.display = 'none';
+    list.className = 'nav-link';
+    sectionAddBook.style.display = 'block';
+    addNew.className = 'nav-link active';
+    sectionContact.style.display = 'none';
+    contact.className = 'nav-link';
+  };
+  
+  export default navigateAddNew;
